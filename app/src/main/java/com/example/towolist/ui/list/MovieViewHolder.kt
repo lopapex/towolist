5a60d59641547a96e907016657077df0eb366826
Matěj Novotný <xnovot19@fi.muni.cz>
2022-04-06 23:48:20 +0200

    Adding placeholders to glide load

--- app/src/main/java/com/example/towolist/ui/list/MovieViewHolder.kt
+++ app/src/main/java/com/example/towolist/ui/list/MovieViewHolder.kt
@@ -3,6 +3,7 @@ package com.example.towolist.ui.list
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.towolist.R
 import com.example.towolist.databinding.ItemListBinding
 import com.example.towolist.ui.data.MovieItem
 
@@ -13,6 +14,9 @@ class MovieViewHolder(private val binding: ItemListBinding, private val view: Vi
         binding.mediaName.text = movieItem.name
         Glide.with(view.context)
             .load(movieItem.imageSource)
+            .placeholder(R.drawable.empty_image)
+            .error(R.drawable.empty_image)
+            .fallback(R.drawable.empty_image)
             .into(binding.poster)
 
         binding.cardContainer.setOnClickListener {
