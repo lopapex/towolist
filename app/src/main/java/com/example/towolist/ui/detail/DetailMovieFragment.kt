f36fd75baa3d5b9d8446c9d59bb64d94f3eda143
Matěj Novotný <xnovot19@fi.muni.cz>
2022-04-11 18:30:33 +0200

    Update detail fragment

--- app/src/main/java/com/example/towolist/ui/detail/DetailMovieFragment.kt
+++ app/src/main/java/com/example/towolist/ui/detail/DetailMovieFragment.kt
@@ -4,19 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.example.towolist.R
+import com.example.towolist.databinding.FragmentDetailMovieBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-
 class DetailMovieFragment : BottomSheetDialogFragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+
+    private lateinit var binding: FragmentDetailMovieBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentDetailMovieBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_movie, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.mediaName.text = "The Batman"
+        binding.options.text = "Watch Options"
+        binding.watchNow.text = "Watch Now"
+
+        Glide.with(view.context)
+            .load("https://image.tmdb.org/t/p/w300_and_h450_bestv2/74xTEgt7R36Fpooo50r9T25onhq.jpg")
+            .placeholder(R.drawable.empty_image)
+            .error(R.drawable.empty_image)
+            .fallback(R.drawable.empty_image)
+            .into(binding.poster)
     }
 }
