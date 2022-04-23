1f562b90339c2fdc1c63e8cff48169f319a448f7
Matěj Novotný <xnovot19@fi.muni.cz>
2022-04-23 23:06:52 +0200

    Remove toast

--- app/src/main/java/com/example/towolist/ui/list/ListFragment.kt
+++ app/src/main/java/com/example/towolist/ui/list/ListFragment.kt
@@ -242,7 +242,6 @@ class ListFragment : Fragment(), IUpdateLayoutFragment, MaterialSearchBar.OnSear
             }
         )
 
-        Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
         mainActivity.getSearchBar().closeSearch()
     }
 
