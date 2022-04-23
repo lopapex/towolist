66b49cfb5be790cba5d0dda5c84dbc289aa32ef9
Matěj Novotný <xnovot19@fi.muni.cz>
2022-04-23 16:35:42 +0200

    Add  redirectiong demo

---
+++ app/src/main/java/com/example/towolist/data/ServiceInfo.kt
@@ -0,0 +1,4 @@
+package com.example.towolist.data
+
+enum class ServiceInfo {
+}
--- app/src/main/java/com/example/towolist/ui/detail/DetailMovieFragment.kt
+++ app/src/main/java/com/example/towolist/ui/detail/DetailMovieFragment.kt
@@ -1,10 +1,13 @@
 package com.example.towolist.ui.detail
 
 import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView.OnItemClickListener
 import android.widget.ImageButton
 import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
@@ -13,6 +16,7 @@ import com.example.towolist.data.MovieItem
 import com.example.towolist.databinding.FragmentDetailMovieBinding
 import com.example.towolist.utils.getFormattedDateString
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
 
 class DetailMovieFragment() : BottomSheetDialogFragment() {
 
@@ -39,6 +43,16 @@ class DetailMovieFragment() : BottomSheetDialogFragment() {
 
         updateImageButtonColor(binding.watchedIcon, item.isWatched, view)
         updateImageButtonColor(binding.toWatchIcon, item.isToWatch, view)
+        binding.watchNowList.onItemClickListener = OnItemClickListener { parent, v, position, id ->
+            try {
+                val intent = v.context.packageManager.getLaunchIntentForPackage("com.hbo.hbonow")
+                startActivity(intent)
+            } catch (e: Exception) {
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse("http://www.netflix.com/watch")
+                startActivity(intent)
+            }
+        }
 
         updateBasedOnServices(item)
     }
