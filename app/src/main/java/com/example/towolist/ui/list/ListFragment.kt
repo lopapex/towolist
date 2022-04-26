4b3366221d8a226ae76563189925de3dc4dc1061
Matěj Novotný <xnovot19@fi.muni.cz>
2022-04-26 16:55:04 +0200

    Remove unused imports

--- app/src/main/java/com/example/towolist/ui/list/ListFragment.kt
+++ app/src/main/java/com/example/towolist/ui/list/ListFragment.kt
@@ -5,9 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.InputMethodManager
-import android.widget.AdapterView
-import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.fragment.app.Fragment
